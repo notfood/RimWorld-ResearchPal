6530d0d51d645104db8a8ebde5cfa44497cc5048
notfood <notfood@users.noreply.github.com>
2018-08-29 23:55:59 -0500

    Add ResearchPal's denser layout
    It's an option. Can interchange as needed but requires Rimworld to Restart
    - TODO: Make it possible to rebuild the tree after user changes the
    layout.

---
+++ Source/ResourceBank.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+
+namespace FluffyResearchTree
+{
+  public static class ResourceBank
+  {
+        public static class String {
+            #region Settings
+            public static readonly string ShowNotificationPopup = "Fluffy.ResearchTree.ShowNotificationPopup".Translate();
+            public static readonly string ShowNotificationPopupTip = "Fluffy.ResearchTree.ShowNotificationPopupTip".Translate();
+
+            public static readonly string ShouldSeparateByTechLevels = "Fluffy.ResearchTree.ShouldSeparateByTechLevels".Translate();
+            public static readonly string ShouldSeparateByTechLevelsTip = "Fluffy.ResearchTree.ShouldSeparateByTechLevelsTip".Translate();
+
+            public static readonly string ShouldPauseOnOpen = "Fluffy.ResearchTree.ShouldPauseOnOpen".Translate();
+            public static readonly string ShouldPauseOnOpenTip = "Fluffy.ResearchTree.ShouldPauseOnOpenTip".Translate();
+            public static readonly string ShouldResetOnOpen = "Fluffy.ResearchTree.ShouldResetOnOpen".Translate();
+            public static readonly string ShouldResetOnOpenTip = "Fluffy.ResearchTree.ShouldResetOnOpenTip".Translate();
+
+            public static readonly string DebugResearch = "Fluffy.ResearchTree.DebugResearch".Translate();
+            public static readonly string DebugResearchTip = "Fluffy.ResearchTree.DebugResearchTip".Translate();
+            #endregion
+
+            public static readonly string NeedsRestart = "Fluffy.ResearchTree.NeedsRestart".Translate();
+        }
+  }
+}
